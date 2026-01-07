51f30b3f744140c406e5b2f65ad465437deffc22
Igor Franca <igor.franca@liferay.com>
2026-01-06 22:30:46 -0300

    LPD-73635 [BE][FE] Create project info summary

--- modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/js/index.ts
+++ modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/js/index.ts
@@ -3,5 +3,6 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+export {default as ProjectInfoSummary} from './main_view/projects/components/ProjectInfoSummary';
 export {default as ProjectsFDSPropsTransformer} from './main_view/props_transformer/ProjectsFDSPropsTransformer';
 export {default as ProjectEditorToolbar} from './project_editor/components/ProjectEditorToolbar';
