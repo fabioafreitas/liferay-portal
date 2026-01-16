5b3ac3a4471e0aa78f3387abe1f132dd6711da16
Kevin Tan <kevin.tan@liferay.com>
2026-01-15 18:14:41 -0800

    LPD-75958 Filter FDS results using liferay state

--- modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/js/main_view/props_transformer/TasksFDSPropsTransformer.tsx
+++ modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/js/main_view/props_transformer/TasksFDSPropsTransformer.tsx
@@ -13,6 +13,7 @@ import {
 } from '@liferay/site-cms-site-initializer';
 
 import StateLabel from '../../common/components/StateLabel';
+import {cmpTasksFDSAtom} from './atoms';
 
 type action = {
 	data: {
@@ -67,6 +68,7 @@ export default function TasksFDSPropsTransformer({
 }) {
 	return {
 		...otherProps,
+		atom: cmpTasksFDSAtom,
 		creationMenu: {
 			...creationMenu,
 			primaryItems: addOnClickToCreationMenuItems(
