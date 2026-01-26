f53e65e787dd09e1e248db36af05db30f6e6b335
Fábio Alves <fabio.alves@liferay.com>
2026-01-26 13:54:17 -0300

    LPD-75946 [FE] Add modal to create tasks and projects when none exist on global task list page

--- modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/js/components/props_transformer/actions/createTaskAction.ts
+++ modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/js/components/props_transformer/actions/createTaskAction.ts
@@ -5,12 +5,41 @@
 
 import {navigate} from 'frontend-js-web';
 
+import {openCMPModal} from '../../../utils/openCMPModal';
+import SelectProjectModalContent from '../../modal/SelectProjectModalContent';
+
 export type Data = {
-	redirect: string;
+	addProjectURL?: string;
+	addTaskURL?: string;
+	projectObjectDefinitionId: string;
+	redirect?: string;
 };
 
 export default function createTaskAction(data: Data) {
-	const url = new URL(data.redirect);
+	if (!data.addProjectURL && !data.addTaskURL && data.redirect) {
+		const url = new URL(data.redirect);
 
-	navigate(url.pathname + url.search);
+		navigate(url.pathname + url.search);
+
+		return;
+	}
+
+	if (!data.addProjectURL || !data.addTaskURL) {
+		throw new Error(
+			'Both addProjectURL and addTaskURL are required to open the modal'
+		);
+	}
+
+	openCMPModal({
+		center: true,
+		contentComponent: ({closeModal}: {closeModal: () => void}) =>
+			SelectProjectModalContent({
+				...data,
+				addProjectURL: data.addProjectURL!,
+				addTaskURL: data.addTaskURL!,
+				closeModal,
+				projectObjectDefinitionId: data.projectObjectDefinitionId,
+			}),
+		size: 'md',
+	});
 }
