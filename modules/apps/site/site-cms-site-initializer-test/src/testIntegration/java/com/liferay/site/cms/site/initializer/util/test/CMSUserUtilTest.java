57c7aa0f011ad9a48ce9bee2b4bca98979265224
Fábio Alves <fabio.alves@liferay.com>
2026-02-11 15:09:04 -0300

    LPD-78045 Update integration test class

--- modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/util/test/CMSUserUtilTest.java
+++ modules/apps/site/site-cms-site-initializer-test/src/testIntegration/java/com/liferay/site/cms/site/initializer/util/test/CMSUserUtilTest.java
@@ -24,12 +24,13 @@ import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.test.rule.FeatureFlag;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 import com.liferay.site.cms.site.initializer.test.util.CMSTestUtil;
-import com.liferay.site.cms.site.initializer.util.UserSelectionFDSFilterUtil;
+import com.liferay.site.cms.site.initializer.util.CMSUserUtil;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -44,7 +45,7 @@ import org.junit.runner.RunWith;
  */
 @FeatureFlag("LPD-17564")
 @RunWith(Arquillian.class)
-public class UserSelectionFDSFilterUtilTest {
+public class CMSUserUtilTest {
 
 	@ClassRule
 	@Rule
@@ -82,7 +83,7 @@ public class UserSelectionFDSFilterUtilTest {
 
 		_setUser(user1);
 
-		Assert.assertNull(UserSelectionFDSFilterUtil.getUsers());
+		Assert.assertTrue(SetUtil.isEmpty(CMSUserUtil.getUsers(null)));
 
 		_userLocalService.addGroupUser(
 			depotEntry1.getGroupId(), user1.getUserId());
@@ -129,7 +130,7 @@ public class UserSelectionFDSFilterUtilTest {
 		Assert.assertTrue(
 			ArrayUtil.containsAll(
 				TransformUtil.transformToLongArray(
-					UserSelectionFDSFilterUtil.getUsers(), User::getUserId),
+					CMSUserUtil.getUsers(null), User::getUserId),
 				expectedUserIds));
 	}
 
