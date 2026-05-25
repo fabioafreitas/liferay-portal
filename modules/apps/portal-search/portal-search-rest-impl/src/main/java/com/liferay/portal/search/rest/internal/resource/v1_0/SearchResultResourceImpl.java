1615c2174fe39c4d7f4c05cf35346975058c9693
Fábio Alves <fabio.alves@liferay.com>
2026-05-25 07:42:18 -0300

    LPD-88846 WIP search team suggestions

--- modules/apps/portal-search/portal-search-rest-impl/src/main/java/com/liferay/portal/search/rest/internal/resource/v1_0/SearchResultResourceImpl.java
+++ modules/apps/portal-search/portal-search-rest-impl/src/main/java/com/liferay/portal/search/rest/internal/resource/v1_0/SearchResultResourceImpl.java
@@ -381,16 +381,9 @@ public class SearchResultResourceImpl extends BaseSearchResultResourceImpl {
 			).size(
 				pagination.getPageSize()
 			).withSearchContext(
-				searchContext -> {
-					_populateSearchContext(
-						searchRequestBody.getAttributes(), filter, scope,
-						search, searchContext, sorts);
-
-					if (_isEmbedded()) {
-						searchContext.setAttribute(
-							"search.permission.filter.forced", Boolean.TRUE);
-					}
-				}
+				searchContext -> _populateSearchContext(
+					searchRequestBody.getAttributes(), filter, scope, search,
+					searchContext, sorts)
 			);
 
 		String[] entryClassNamesArray = ValueUtil.toArray(entryClassNames);
